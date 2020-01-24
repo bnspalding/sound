150a557dafd9f09c674a3fa21ccc11482b26b1f3
Ben Spalding <bnspalding418@gmail.com>
2020-01-24 11:25:51 -0500

    documentation for rhyme strict and approx

--- src/Rhyme/Approx.hs
+++ src/Rhyme/Approx.hs
@@ -1,18 +1,28 @@
 -- |
 -- Module: Rhyme.Approx
 -- Description: approximate rhyme
--- Copyright: (c) 2020 Ben Spalding (bnspalding.com)
+-- Copyright: (c) 2019 Ben Spalding (bnspalding.com)
 -- License: MIT
--- Stability: Experimental
+-- Stability: experimental
 --
 -- Rhyme.Approx provides tools for determing (approximately) how close two
 -- syllables are by measure of rhyme, assonance, or alliteration. In all cases,
 -- similarity between syllables is based on what fraction of the total features
--- of the two syllables' sounds overlap
+-- of the two syllables' sounds overlap.
+--
+-- The feature sets for sounds are taken from "Sound.GenAm", which means that the
+-- tools are currently constrained to a particular, semi-arbitrary mapping.
+--
+-- Currently, ordering of sounds is not considered in measuring the similarity.
+-- Instead, a \'bag of sounds\' approach is taken, and this should be taken into
+-- consideration when evaluating what similarity means.
 module Rhyme.Approx
-  ( rhyme,
+  ( -- * Measures
+    rhyme,
     assonance,
     alliteration,
+
+    -- * Similarity
     similarity,
   )
 where
@@ -23,15 +33,27 @@ import Sound.Feature
 import qualified Sound.GenAm as GenAm
 import qualified Sound.Syl as Syl
 
+-- | rhyme generates a measure of similarity between two syllables by comparing
+-- the rhymes of the syllables (nucleus + coda) together as flat lists.
 rhyme :: Syl -> Syl -> Float
 rhyme = _similarity Syl.rhyme
 
+-- | assonance generates a measure of similarity between two syllables by
+-- comparing only their nuclei. The nucleus is the most sonorous sound
+-- (generally a vowel) at the center of a syllable.
 assonance :: Syl -> Syl -> Float
 assonance = _similarity Syl.nucleus
 
+-- | alliteration generates a measure of similarity between two syllables by
+-- comparing their onsets. The onset is the cluster of sounds that begin a
+-- syllable, up until the nucleus.
 alliteration :: Syl -> Syl -> Float
 alliteration = _similarity Syl.onset
 
+-- | similarity compares two sets of sounds and returns a measure of similarity
+-- (a number between 0 and 1). The similarity measure is the fraction of
+-- features shared by the two sets of sounds over the total number of unique
+-- sounds in the two sound sets.
 similarity :: [Sound] -> [Sound] -> Float
 similarity ss1 ss2 =
   fromIntegral (Set.size $ Set.intersection fs1 fs2)
@@ -48,3 +70,6 @@ _featuresOf ss = featuresOrEmpty . GenAm.features <$> ss
 
 _merge :: [FeatureSet] -> FeatureSet
 _merge = foldl Set.union Set.empty
+-- TODO: revisit the type of number that should be returned by similarity. is it
+-- a ratio? a broader typeclass? Look at what is both most conventional and what
+-- is most correct.
--- src/Rhyme/Strict.hs
+++ src/Rhyme/Strict.hs
@@ -1,21 +1,29 @@
 -- |
--- Module: N
--- Description: Short Description
--- Copyright: (c) 2020 Ben Spalding (bnspalding.com)
+-- Module: Rhyme.Strict
+-- Description: strict rhyme
+-- Copyright: (c) 2019 Ben Spalding (bnspalding.com)
 -- License: MIT
--- Stability: Experimental
+-- Stability: experimental
 --
--- Longer description
+-- Rhyme.Strict provides tools for determing if two syllables match by measure
+-- of rhyme, assonance, or alliteration. In all cases, a subset of sounds from
+-- the syllables, selected by the measure used, are compared for equality. Two
+-- syllables either match by some measure, or they do not.
 module Rhyme.Strict where
 
 import Sound
 import qualified Sound.Syl as Syl
 
+-- | rhyme compares the rhymes (nucleus + coda) of two syllables to each other.
 rhyme :: Syl -> Syl -> Bool
 rhyme syl1 syl2 = Syl.rhyme syl1 == Syl.rhyme syl2
 
+-- | assonance compares the nuclei of two syllables to each other. The nucelus
+-- is the most sonorous sound (generally a vowel) at the center of a syllable.
 assonance :: Syl -> Syl -> Bool
 assonance syl1 syl2 = Syl.nucleus syl1 == Syl.nucleus syl2
 
+-- | alliteration compares the onsets of two syllables to each other. The onset
+-- is the cluster of sounds that begin a syllable, up until the nucleus.
 alliteration :: Syl -> Syl -> Bool
 alliteration syl1 syl2 = Syl.onset syl1 == Syl.onset syl2
