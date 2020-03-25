249c21a8f3d38dad255090abfc85f2e0c0004f48
Ben Spalding <bnspalding418@gmail.com>
2020-03-24 21:23:38 -0400

    ø -> ʊ

--- src/Sound/GenAm/IPA.hs
+++ src/Sound/GenAm/IPA.hs
@@ -72,6 +72,7 @@ replZero =
       Replace (s " ̯") "",
       Replace (s "u̯") "u",
       Replace (s "ʊ̯") "ʊ",
+      Replace (s "ø") "ʊ", -- I'm torn on this judgement
       Replace (s "ô") "o",
       Replace (s "ə̯") "ə",
       Replace (s "ɪ̯") "ɪ",
